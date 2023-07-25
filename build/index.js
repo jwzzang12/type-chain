d36b18d661909a1910d665562c7dcdfc970199d1
jwzzang12 <moonbb12@gmail.com>
2023-07-25 10:15:50 +0100

    Set compilerOption manually

---
+++ build/index.js
@@ -0,0 +1,7 @@
+//compiled to more compatible version of JS (ES3)
+// var hello = function () {
+//   return 'hi';
+// };
+
+//after change complierOptions into ES6 in tsconfig.json
+const hello = () => 'hi';
